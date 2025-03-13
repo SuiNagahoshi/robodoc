444bd0467fd1a13bf87a72b96c2a4b4d33852b32
Sui Nagahoshi <119501407+SuiNagahoshi@users.noreply.github.com>
2025-03-13 14:30:46 +0900

    feat: added subcommands for generating and loading configs

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 mod config;
+use crate::config::structure::Config;
 
 use std::env;
+use std::path;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -9,8 +11,12 @@ fn main() {
     }
     if args[1] == "init" {
         println!("Initializing...");
+        let config = Config::new();
+        config.generate(&path::PathBuf::from("config.toml"));
     } else if args[1] == "generate" {
         println!("Generating...");
+        let config = Config::import(&path::PathBuf::from("config.toml"));
+        println!("{:?}", config);
     } else {
         eprintln!("Argument error! Please provide one argument.\n'init', 'generate'");
     }
